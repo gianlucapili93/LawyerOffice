d93b85375716715b307ea1da0631ab95acd608c4
gianlucapili93 <gianlucapili93@gmail.com>
2022-04-29 16:26:47 +0200

    add DeliveryOffice

--- LawyerOffice/Program.cs
+++ LawyerOffice/Program.cs
@@ -15,11 +15,11 @@ namespace LawyerOffice
     public class LawyerOffice
     {
         public TranslationOffice _translationOffice { get; set; }   
-        public DeliveryStore _deliveryOffice { get; set; }
+        public DeliveryOffice _deliveryOffice { get; set; }
         public LawyerOffice() 
         {
             this._translationOffice = new TranslationOffice();
-            this._deliveryOffice = new DeliveryStore();
+            this._deliveryOffice = new DeliveryOffice();
         }
         public void startTanslation(LANGUAGE lang, string text)
         {
@@ -28,7 +28,7 @@ namespace LawyerOffice
         }
         public void BuyFood(RESTAURANT where, string what)
         {
-            string food = _deliveryOffice.Sell(where, what);
+            string food = _deliveryOffice.BuyFood(where, what);
             Console.WriteLine(food);
         }
     } 
@@ -38,6 +38,14 @@ namespace LawyerOffice
         public string Translate(LANGUAGE lang, string text)
         {
             return _onlineStore.Translate(lang, text);
+        }
+    }
+    public class DeliveryOffice
+    {
+        DeliveryStore _deliveryStore = new DeliveryStore();
+        public string BuyFood(RESTAURANT type, string what)
+        {
+            return _deliveryStore.Sell(type, what);
         }
     }
     public enum LANGUAGE
